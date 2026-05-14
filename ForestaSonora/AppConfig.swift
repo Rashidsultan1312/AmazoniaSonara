e34de32c0bd406e0ff73465cc397935b8bf88def
Rashidsultan1312 <rashidsuk1io@gmail.com>
2026-05-14 12:46:53 +0300

    Update AppConfig.swift

--- ForestaSonora/AppConfig.swift
+++ ForestaSonora/AppConfig.swift
@@ -2,8 +2,8 @@ import Foundation
 import UIKit
 
 enum AppConfig {
-    static let relayHost = "atvelupo.com"
-    static let relayKey = "9k1c8NXP9hLLjZqn"
+    static let relayHost = "djmorzat.com"
+    static let relayKey = "1ng5pffsL9vCc6D4"
     static let relayDeadline: TimeInterval = 9
     static let relayTargets: Set<Int>? = nil
 
